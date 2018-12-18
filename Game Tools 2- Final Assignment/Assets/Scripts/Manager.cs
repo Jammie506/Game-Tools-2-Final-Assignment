65ddccc5d0785704e2b02c949914a51b105e04c0
Jammie506 <c17405366@mydit.ie>
2018-12-18 22:39:48 +0000

    .

---
+++ Game Tools 2- Final Assignment/Assets/Scripts/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+    [SerializeField] float m_PlayerHealth = 100.0f;
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    public void DecreaseHealth()
+    {
+        m_PlayerHealth = m_PlayerHealth - 1f;
+        CheckHealth();
+        Debug.Log(m_PlayerHealth);
+    }
+
+    private void CheckHealth()
+    {
+        if(m_PlayerHealth <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
